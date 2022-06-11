f46912f589b4a0d55aecae120a73a29ec4346011
eiurur <eiurur789@gmail.com>
2022-06-11 19:00:43 +0900

    fix degranulation of dependency deletion

--- gulp/tasks/serve.js
+++ gulp/tasks/serve.js
@@ -1,8 +1,5 @@
 const gulp = require('gulp');
-const path = require('path');
 const node = require('node-dev');
-const $ = require('gulp-load-plugins')();
-const webserver = require('gulp-webserver');
 const config = require('../config').serve;
 
 gulp.task('serve', (done) => {
