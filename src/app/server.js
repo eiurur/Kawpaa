c4ee54505b1dceea14891396e3c4217022b7c44a
eiurur <eiurur789@gmail.com>
2022-06-11 19:01:06 +0900

    add swtich for http/https server

--- src/app/server.js
+++ src/app/server.js
@@ -1,8 +1,11 @@
 const fs = require('fs');
 const path = require('path');
+const { my } = require(path.resolve('build', 'lib', 'my'));
 
 module.exports = (listener) => {
   try {
+    if (my.toBoolean(process.env.FORCE_HTTP)) throw new Error('Forcing HTTP');
+
     const hskey = fs.readFileSync(path.resolve('keys', 'server.key'));
     const hscert = fs.readFileSync(path.resolve('keys', 'server.crt'));
 
