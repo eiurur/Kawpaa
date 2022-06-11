0e60bd6aa83d1428b76fe4d5b9072f5823383917
eiurur <eiurur789@gmail.com>
2022-06-11 16:39:53 +0900

    fix build errors related to node-sass

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -3,20 +3,28 @@
  * DS102: Remove unnecessary code created because of implicit returns
  * Full docs: https://github.com/decaffeinate/decaffeinate/blob/master/docs/suggestions.md
  */
-const gulp   = require('gulp');
-const sass   = require('gulp-sass');
-// sass   = require 'gulp-ruby-sass'
-const $      = (require('gulp-load-plugins'))();
+const gulp = require('gulp');
+const dartSass = require('sass');
+const gulpSass = require('gulp-sass');
+const sass = gulpSass(dartSass);
+const $ = require('gulp-load-plugins')();
 const config = require('../config').sass;
 
 // SASS (src/scss)
-gulp.task("sass", () => gulp.src(config.src)
-  .pipe($.plumber())
-  .pipe(sass({
-    outputStyle: "expanded"})).pipe(gulp.dest(config.dest))
-  .pipe($.rename({suffix: '.min'}))
-  .pipe($.cssmin())
-  .pipe(gulp.dest(config.dest))
-  .pipe($.gzip())
-  .pipe(gulp.dest(config.dest))
-  .pipe($.notify('CSS task complete')));
+gulp.task('sass', () =>
+  gulp
+    .src(config.src)
+    .pipe($.plumber())
+    .pipe(
+      sass({
+        outputStyle: 'expanded',
+      })
+    )
+    .pipe(gulp.dest(config.dest))
+    .pipe($.rename({ suffix: '.min' }))
+    .pipe($.cssmin())
+    .pipe(gulp.dest(config.dest))
+    .pipe($.gzip())
+    .pipe(gulp.dest(config.dest))
+    .pipe($.notify('CSS task complete'))
+);
